e3c59d375f47ed1d7c04731c337b18d69b5c5146
김범석 <kqjatjr@gmail.com>
2021-11-09 03:55:26 +0900

    useReducer - Info2(미완성)

---
+++ src/Components/Info2.tsx
@@ -0,0 +1,45 @@
+import React, { useReducer } from "react";
+
+type DemoType = {
+  name: string;
+  nickname: string;
+};
+
+const nameReducer = (state: DemoType, action: Action) => {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+};
+
+const Info2 = () => {
+  const [state, dispatch] = useReducer(nameReducer, {
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = state;
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(event.target);
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름: </b> {name}
+        </div>
+        <div>
+          <b>닉네임 : </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info2;
